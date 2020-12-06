c734f4af4fb3c8a7d783c6d1e2a5babc798caed4
yasgo <yasinburakkalkan@gmail.com>
2020-12-06 13:30:44 +0300

    Copy code widget development.

--- src/components/code-widget/index.jsx
+++ src/components/code-widget/index.jsx
@@ -1,13 +1,32 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { FormattedMessage } from 'react-intl'
 
 const CodeWidget = props => {
-
     const code = useRef(null);
 
-    const copyTextClipboard = () => navigator.clipboard.writeText(code.current.innerText)
+    let [isCopied, setIsCopied] = useState(false);
+
+    const copyTextClipboard = () => {
+        if (!isCopied) {
+            setIsCopied(true)
+            navigator.clipboard.writeText(code.current.innerText)
+            resetCopiedInfo()
+        }
+    }
+
+    const resetCopiedInfo = () => {
+        let resetInterval = setInterval(() => {
+            setIsCopied(false)
+
+            clearInterval(resetInterval)
+        }, 3000);
+    }
 
     return (
-        <div className="code-widget" onClick={copyTextClipboard}>
+        <div className={`code-widget${isCopied ? ' copied' : ''}`} onClick={copyTextClipboard}>
+            <div className="copy-message">
+                <FormattedMessage id='copied.info' defaultMessage="Copied text to clipboard." />
+            </div>
             <code ref={code}>{props.code}</code>
             <i className="icon-copy"></i>
         </div>
