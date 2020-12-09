67ffbc0db576401de97ab416ddb9db6238a09331
yasgo <yasinburakkalkan@gmail.com>
2020-12-09 19:05:31 +0300

    Home page language development.

--- src/components/home/development-environments/index.jsx
+++ src/components/home/development-environments/index.jsx
@@ -1,14 +1,30 @@
-import DevelopmentEnvironmentItem from './item';
-import { Row, Col } from 'react-bootstrap'
+import DevelopmentEnvironmentItem from "./item";
+import { FormattedMessage } from "react-intl";
+import { Row, Col } from "react-bootstrap"
 
 const DevelopmentEnvironments = props => {
     return (
         <Row>
             <Col md={{ span: 10, offset: 1 }}>
                 <div className="development-environments-section">
-                    <DevelopmentEnvironmentItem icon="codacy" color="indigo" title="Codacy" detail="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
-                    <DevelopmentEnvironmentItem icon="github" color="aliceblue" title="Github" detail="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
-                    <DevelopmentEnvironmentItem icon="npm" color="guardsmanred" title="NPM" detail="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
+                    <DevelopmentEnvironmentItem
+                        icon="codacy"
+                        color="indigo"
+                        title="Codacy"
+                        detail={<FormattedMessage id="home.environments.codacy.detail" defaultMessage="As fast as a Ferrari" />}
+                    />
+                    <DevelopmentEnvironmentItem
+                        icon="github"
+                        color="aliceblue"
+                        title="Github"
+                        detail={<FormattedMessage id="home.environments.github.detail" defaultMessage="As fast as a Ferrari" />}
+                    />
+                    <DevelopmentEnvironmentItem
+                        icon="npm"
+                        color="guardsmanred"
+                        title="NPM"
+                        detail={<FormattedMessage id="home.environments.npm.detail" defaultMessage="As fast as a Ferrari" />}
+                    />
                 </div>
             </Col>
         </Row>
--- src/components/home/services/index.jsx
+++ src/components/home/services/index.jsx
@@ -1,29 +1,60 @@
-import { Row, Col } from 'react-bootstrap'
-import ServicesItem from './item'
+import { Row, Col } from "react-bootstrap";
+import { FormattedMessage } from "react-intl";
+import ServicesItem from "./item"
 
 const Services = props => {
     return (
         <div className="services-section">
             <Row>
                 <Col md={4}>
-                    <ServicesItem icon="rocket" color="sunglo" title="As fast as a Ferrari" detail="High performance and quality code." />
+                    <ServicesItem
+                        icon="rocket"
+                        color="sunglo"
+                        title={<FormattedMessage id="home.services.rocket.title" defaultMessage="As fast as a Ferrari" />}
+                        detail={<FormattedMessage id="home.services.rocket.detail" defaultMessage="High performance and quality code." />}
+                    />
                 </Col>
                 <Col md={4}>
-                    <ServicesItem icon="cogs" color="portage" title="Simple to use" detail="Easy to use and understandable options." />
+                    <ServicesItem
+                        icon="cogs"
+                        color="portage"
+                        title={<FormattedMessage id="home.services.cogs.title" defaultMessage="Simple to use" />}
+                        detail={<FormattedMessage id="home.services.cogs.detail" defaultMessage="Easy to use and understandable options." />}
+                    />
                 </Col>
                 <Col md={4}>
-                    <ServicesItem icon="brush" color="tanhide" title="UI/UX" detail="User friendly interface." />
+                    <ServicesItem
+                        icon="brush"
+                        color="tanhide"
+                        title={<FormattedMessage id="home.services.brush.title" defaultMessage="UI/UX" />}
+                        detail={<FormattedMessage id="home.services.brush.detail" defaultMessage="User friendly interface." />}
+                    />
                 </Col>
             </Row>
             <Row>
                 <Col md={4}>
-                    <ServicesItem icon="open-source" color="periwinkle" title="Open source" detail="You can review my code and support me." />
+                    <ServicesItem
+                        icon="open-source"
+                        color="periwinkle"
+                        title={<FormattedMessage id="home.services.openSource.title" defaultMessage="Open source" />}
+                        detail={<FormattedMessage id="home.services.openSource.detail" defaultMessage="You can review my code and support me." />}
+                    />
                 </Col>
                 <Col md={4}>
-                    <ServicesItem icon="book" color="algaegreen" title="Documentation" detail="Descriptive and easy to read documentation." />
+                    <ServicesItem
+                        icon="book"
+                        color="algaegreen"
+                        title={<FormattedMessage id="home.services.book.title" defaultMessage="Documentation" />}
+                        detail={<FormattedMessage id="home.services.book.detail" defaultMessage="Descriptive and easy to read documentation." />}
+                    />
                 </Col>
                 <Col md={4}>
-                    <ServicesItem icon="design" color="chenin" title="Design" detail="You can use our own form theme if you wish." />
+                    <ServicesItem
+                        icon="design"
+                        color="chenin"
+                        title={<FormattedMessage id="home.services.design.title" defaultMessage="Design" />}
+                        detail={<FormattedMessage id="home.services.design.detail" defaultMessage="You can use our own form theme if you wish." />}
+                    />
                 </Col>
             </Row>
         </div>
