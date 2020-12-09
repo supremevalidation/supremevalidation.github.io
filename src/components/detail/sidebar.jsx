e5da6aa3a0409945504bbf92b160992ffd9c5b1d
yasgo <yasinburakkalkan@gmail.com>
2020-12-09 10:31:58 +0300

    Sidebar development.

--- src/components/detail/sidebar.jsx
+++ src/components/detail/sidebar.jsx
@@ -1,41 +1,27 @@
 import { Link } from 'react-router-dom'
+import MenuData from '../../data/sidebar'
 
 const Sidebar = props => {
     return (
         <aside>
-            <h4>GETTING STARTED</h4>
-            <ul>
-                <li>
-                    <Link to="/installation">Installation</Link>
-                </li>
-                <li>
-                    <Link to="/usage">Usage</Link>
-                </li>
-            </ul>
-            <h4>API</h4>
-            <ul>
-                <li>
-                    <Link to="/options">Options</Link>
-                </li>
-                <li>
-                    <Link to="/classes">Classes</Link>
-                </li>
-                <li>
-                    <Link to="/attributes">Attributes</Link>
-                </li>
-                <li>
-                    <Link to="/events">Events</Link>
-                </li>
-            </ul>
-            <h4>SUPPORT</h4>
-            <ul>
-                <li>
-                    <Link to="/browser">Browser</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
+            {
+                MenuData.map((item, index) => {
+                    return (
+                        <ul key={index}>
+                            <li><h4>{item.title}</h4></li>
+                            {
+                                item.links.map((subItem, subIndex) => {
+                                    return (
+                                        <li key={subIndex}>
+                                            <Link to={subItem.url}>{subItem.title}</Link>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    )
+                })
+            }
         </aside>
     )
 }
---
+++ src/data/sidebar.jsx
@@ -0,0 +1,51 @@
+const menu = [
+    {
+        title: "GETTING STARTED",
+        links: [
+            {
+                title: "Installation",
+                url: "/installation"
+            },
+            {
+                title: "Usage",
+                url: "/usage"
+            }
+        ]
+    },
+    {
+        title: "API",
+        links: [
+            {
+                title: "Options",
+                url: "/options"
+            },
+            {
+                title: "Classes",
+                url: "/classes"
+            },
+            {
+                title: "Attributes",
+                url: "/attributes"
+            },
+            {
+                title: "Events",
+                url: "/events"
+            }
+        ]
+    },
+    {
+        title: "SUPPORT",
+        links: [
+            {
+                title: "Browser",
+                url: "/browser"
+            },
+            {
+                title: "Contact",
+                url: "/contact"
+            }
+        ]
+    }
+]
+
+export default menu;
