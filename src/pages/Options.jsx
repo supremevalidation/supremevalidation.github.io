268ce3ce0485551dc50887926dd8d072bac97573
yasgo <yasinburakkalkan@gmail.com>
2020-12-10 13:35:34 +0300

    Language development.

--- src/pages/Options.jsx
+++ src/pages/Options.jsx
@@ -1,25 +1,32 @@
-import Detail from '../components/detail';
+import { FormattedMessage } from "react-intl";
+import Detail from "../components/detail";
 
 const Options = props => {
     return (
         <Detail {...props}>
             <article className="detail-item">
                 <h4>buttonDisabled</h4>
-                <p className="feature"><strong>Type:</strong> Boolean</p>
-                <p className="feature"><strong>Default:</strong> false</p>
-                <p>Place one of the following script near the end of your pages, right before the closing body tag, to enable them.</p>
+                <p className="feature"><strong><FormattedMessage id="sidebar.api.options.type" defaultMessage="Type" />:</strong> Boolean</p>
+                <p className="feature"><strong><FormattedMessage id="sidebar.api.options.default" defaultMessage="Default" />:</strong> false</p>
+                <p>
+                    <FormattedMessage id="sidebar.api.options.buttonDisabled.detail" defaultMessage="Form button is disabled. If success fill form, form button is enabled." />
+                </p>
             </article>
             <article className="detail-item">
                 <h4>keyUpOption</h4>
                 <p className="feature"><strong>Type:</strong> Boolean</p>
                 <p className="feature"><strong>Default:</strong> false</p>
-                <p>Place one of the following script near the end of your pages, right before the closing body tag, to enable them.</p>
+                <p>
+                    <FormattedMessage id="sidebar.api.options.keyUpOption.detail" defaultMessage="If form element is changed, form element is validated." />
+                </p>
             </article>
             <article className="detail-item">
                 <h4>focusOutOption</h4>
                 <p className="feature"><strong>Type:</strong> Boolean</p>
                 <p className="feature"><strong>Default:</strong> false</p>
-                <p>Place one of the following script near the end of your pages, right before the closing body tag, to enable them.</p>
+                <p>
+                    <FormattedMessage id="sidebar.api.options.focusOutOption.detail" defaultMessage="Form element is validated only if focus out." />
+                </p>
             </article>
         </Detail>
     )
--- src/pages/Usage.jsx
+++ src/pages/Usage.jsx
@@ -1,12 +1,15 @@
-import Detail from '../components/detail';
-import CodeWidget from '../components/code-widget';
+import { FormattedMessage } from "react-intl";
+import Detail from "../components/detail";
+import CodeWidget from "../components/code-widget";
 
 const Usage = props => {
     return (
         <Detail {...props}>
             <article className="detail-item">
                 <h4>JS</h4>
-                <p className="mb-4">Place one of the following script near the end of your pages, right before the closing body tag, to enable them. jQuery must come first, then our JavaScript plugin.</p>
+                <p className="mb-4">
+                    <FormattedMessage id="sidebar.gettingStarted.usage.js.detail" defaultMessage="Place one of the following script near the end of your pages, right before the closing body tag, to enable them. jQuery must come first, then our JavaScript plugin." />
+                </p>
                 <CodeWidget>
                     <code>{"<script src=“https://code.jquery.com/jquery-3.5.1.min.js”></script>"}</code>
                     <code>{"<script src=“assets/js/supreme.validation.min.js”></script>"}</code>
@@ -14,7 +17,9 @@ const Usage = props => {
             </article>
             <article className="detail-item">
                 <h4>CSS</h4>
-                <p className="mb-4">Copy-paste the stylesheet into your before all other stylesheets to load our CSS. You can use ready-made theme if you want.</p>
+                <p className="mb-4">
+                    <FormattedMessage id="sidebar.gettingStarted.usage.css.detail" defaultMessage="Copy-paste the stylesheet into your before all other stylesheets to load our CSS. You can use ready-made theme if you want." />
+                </p>
                 <CodeWidget>
                     <code>{"<link href=“assets/css/supreme.validation.min.css” rel=“stylesheet”>"}</code>
                     <code>{"<link href=“assets/css/supreme.validation.theme.min.css” rel=“stylesheet”>"}</code>
@@ -22,7 +27,9 @@ const Usage = props => {
             </article>
             <article className="detail-item">
                 <h4>HTML</h4>
-                <p className="mb-4">You can clone repo from Github and free use in your project. Copy this code and paste code in console.</p>
+                <p className="mb-4">
+                    <FormattedMessage id="sidebar.gettingStarted.usage.html.detail" defaultMessage="You can clone repo from Github and free use in your project. Copy this code and paste code in console." />
+                </p>
                 <CodeWidget>
                     <code>{'<form id="contact-form" class="supreme-validate">'}</code>
                     <code>{'<div class="supreme-validate-element" error-message="Required field">'}</code>
