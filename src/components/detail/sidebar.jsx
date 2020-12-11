faf0c78690256005eaefc916789d6a68f2f86855
yasgo <yasinburakkalkan@gmail.com>
2020-12-11 23:22:05 +0300

    Mobile sidebar development.

--- src/components/detail/sidebar.jsx
+++ src/components/detail/sidebar.jsx
@@ -3,6 +3,31 @@ import { FormattedMessage } from 'react-intl';
 
 const Sidebar = props => (
     <aside>
+        <select id="sidebar" name="sidebar">
+            {
+                props.data.map((item, index) => {
+                    return (
+                        <FormattedMessage id={item.id} defaultMessage={item.title}>
+                            {(title) => {
+                                return (
+                                    <optgroup label={title}>
+                                        {
+                                            item.links.map((subItem, subIndex) => {
+                                                return (
+                                                    <FormattedMessage id={subItem.id} defaultMessage={subItem.title}>
+                                                        {(message) => <option value={subItem.url} selected={props.match.path === subItem.url}>{message}</option>}
+                                                    </FormattedMessage>
+                                                )
+                                            })
+                                        }
+                                    </optgroup>
+                                )
+                            }}
+                        </FormattedMessage>
+                    )
+                })
+            }
+        </select>
         {
             props.data.map((item, index) => {
                 return (
