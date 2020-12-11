98a42b113a40991a51371d59f6392624e4f12178
yasgo <yasinburakkalkan@gmail.com>
2020-12-11 23:39:57 +0300

    Mobile sidebar development.

--- src/components/detail/sidebar.jsx
+++ src/components/detail/sidebar.jsx
@@ -1,52 +1,59 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FormattedMessage } from 'react-intl';
 
-const Sidebar = props => (
-    <aside>
-        <select id="sidebar" name="sidebar">
+const Sidebar = props => {
+
+    let history = useHistory();
+
+    const onSidebarSelectChange = e => history.push(e.target.value)
+
+    return (
+        <aside>
+            <select id="sidebar" name="sidebar" onChange={onSidebarSelectChange} defaultValue={props.match.path}>
+                {
+                    props.data.map((item, index) => {
+                        return (
+                            <FormattedMessage id={item.id} defaultMessage={item.title} key={index}>
+                                {(title) => {
+                                    return (
+                                        <optgroup label={title}>
+                                            {
+                                                item.links.map((subItem, subIndex) => {
+                                                    return (
+                                                        <FormattedMessage id={subItem.id} defaultMessage={subItem.title} key={subIndex}>
+                                                            {(message) => <option value={subItem.url}>{message}</option>}
+                                                        </FormattedMessage>
+                                                    )
+                                                })
+                                            }
+                                        </optgroup>
+                                    )
+                                }}
+                            </FormattedMessage>
+                        )
+                    })
+                }
+            </select>
             {
                 props.data.map((item, index) => {
                     return (
-                        <FormattedMessage id={item.id} defaultMessage={item.title}>
-                            {(title) => {
-                                return (
-                                    <optgroup label={title}>
-                                        {
-                                            item.links.map((subItem, subIndex) => {
-                                                return (
-                                                    <FormattedMessage id={subItem.id} defaultMessage={subItem.title}>
-                                                        {(message) => <option value={subItem.url} selected={props.match.path === subItem.url}>{message}</option>}
-                                                    </FormattedMessage>
-                                                )
-                                            })
-                                        }
-                                    </optgroup>
-                                )
-                            }}
-                        </FormattedMessage>
+                        <ul key={index}>
+                            <li><h4>{<FormattedMessage id={item.id} defaultMessage={item.title} />}</h4></li>
+                            {
+                                item.links.map((subItem, subIndex) => {
+                                    return (
+                                        <li key={subIndex}>
+                                            <Link to={subItem.url} className={props.match.path === subItem.url ? "active" : null}>{<FormattedMessage id={subItem.id} defaultMessage={subItem.title} />}</Link>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
                     )
                 })
             }
-        </select>
-        {
-            props.data.map((item, index) => {
-                return (
-                    <ul key={index}>
-                        <li><h4>{<FormattedMessage id={item.id} defaultMessage={item.title} />}</h4></li>
-                        {
-                            item.links.map((subItem, subIndex) => {
-                                return (
-                                    <li key={subIndex}>
-                                        <Link to={subItem.url} className={props.match.path === subItem.url ? "active" : null}>{<FormattedMessage id={subItem.id} defaultMessage={subItem.title} />}</Link>
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                )
-            })
-        }
-    </aside>
-)
+        </aside>
+    )
+}
 
 export default Sidebar;
